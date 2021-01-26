580f94a691521b4579a028a0b2205b636742c719
hatahata7757 <syouki100241@gmail.com>
2021-01-26 21:16:37 +0900

    update: {プレフィックス}: #{チケット番号} {本文}のルールを追加

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,36 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'ticket-number-check-rule': [2, 'always'],
     'type-enum': [
       2,
       'always',
       [
-        // 必須
-        'add', // 新規機能追加
-        'fix', // （バグ以外の）修正
-        'bugfix', // バグ修正
-        'refactor', // リファクタリング・フォーマット適用
-        'revert', // 変更取り消し
-        'update', // 機能修正（バグではない）
+        'fix', // 開発中のテスト、レビュー指摘の修正
+        'bugfix', // リリース後のバグ修正
+        'add', // 新規（ファイル）機能追加
+        'update', // 機能改修（バグではない）、仕様変更、無効化
+        'clean', //整理（リファクタリング等）
         'remove', // 削除（ファイル）
-        'disable', // 無効化（コメントアウト等）
+        'revert', // 変更取り消し
+        'move' // リソースの移動、名前の変更
       ]
     ]
-  }
+  },
+  plugins: [
+    {
+      rules: {
+        'ticket-number-check-rule': ({ subject }) => {
+          const ACCEPT_PATTERN = '\s#[0-9]+\s';
+          if (subject != null) {
+            return [
+              !subject.match(ACCEPT_PATTERN),
+              'コミットメッセージは{プレフィックス}: #{チケット番号} {本文}の形式にしてください。',
+            ];
+          }
+        }
+      }
+    }
+  ]
+
 };
